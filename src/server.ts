a03d7c3870d7837c90e23ec51d8da5154e4e03f7
Stephanie Ye <stephanie.ye@gmail.com>
2019-02-27 23:05:21 +0000

    database for heroku

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const typeOrmOptions: any = {
     type: "postgres",
     host: connectionOptions.host,
     port: connectionOptions.port,
-    username: "stephslye",
+    username: "",
     password: connectionOptions.password,
     database: connectionOptions.database,
     synchronize: true,
