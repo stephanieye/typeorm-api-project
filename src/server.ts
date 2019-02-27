4858c95e88f202c326200e442aa817b873e22515
Stephanie Ye <stephanie.ye@gmail.com>
2019-02-27 10:18:47 +0000

    added postinstall to run

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('I am listening on port 3000 😸'));
+app.listen(3000, () => console.log('I am listening on port 3000!!! 😸'));
