490bf06d4968ec43ea361abf5e9e423df6b98513
Stephanie Ye <stephanie.ye@gmail.com>
2019-02-27 13:08:16 +0000

    set up postgres and typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import Todo from './entity/Todo';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`I am listening on port ${port}!!! 😸`));
+
+createConnection().then(async (connection) => {
+  const app = express();
+  app.get('/', (req, res) => res.send("This is Stephanie's first backend app with TypeORM and Postgres"));
+  app.get('/create', async (req, res) => {
+    const todo = new Todo();
+    todo.name = 'A Todo';
+    await connection.manager.save(todo);
+    res.send(todo);
+  });
+  app.get('/read', async (req, res) => {
+    const todos = await connection.manager.find(Todo);
+    res.send(todos);
+  });
+  app.listen(port, () => console.log(`I am listening on port ${port}!!! 😸`));
+}).catch((error) => console.log(error));
